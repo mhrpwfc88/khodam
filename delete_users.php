6cd15b322e15c2ba2d79a5fd2f3b7e4923d10904
mhrpwfc88 <mh17pwfc@gmail.com>
2024-08-26 21:19:28 +0700

    style

--- delete_users.php
+++ delete_users.php
@@ -4,10 +4,10 @@ $username = "root"; // Ganti dengan username database Anda
 $password = ""; // Ganti dengan password database Anda
 $dbname = "khodamdb";
 
-// Buat koneksi
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Cek koneksi
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
